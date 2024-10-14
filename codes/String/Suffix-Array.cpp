1dac2cbf5eaabe74fe5d7c2c6d05ae3bf3dcbcb8
temmie <kapoo950807@gmail.com>
2024-10-14 12:36:36 +0800

    換了更快的 SA

--- codes/String/Suffix-Array.cpp
+++ codes/String/Suffix-Array.cpp
@@ -1,68 +1,31 @@
-/*
-s = temmie
-pos = 6 5 1 4 3 2 0 
-rnk = 6 2 5 4 3 1 0 
-
-注意，pos 跟 rnk 的大小為 s.size()+1
-pos:
-(空字串) -> e -> emmie -> ie -> mie -> mmie -> temmie
-rnk[i] = i 在 pos 的哪個位置（第幾小的陣列）
-*/
-
-vector<int> pos, rnk;
-
-// 51f72a
-void Radix_Sort(vector<array<int, 3>> &v){
-    int n = v.size();
-    
-    for (int p=1 ; p>=0 ; p--){
-        vector<int> cnt(n);
-        for (auto x : v){
-            cnt[x[p]]++;
-        }
-
-        vector<array<int, 3>> tmp(n);
-        vector<int> ptr(n); // ptr[i] = 目前 second 是 i 的元素要放在哪個位置
-        ptr[0] = 0;
-        for (int i=1 ; i<n ; i++){
-            ptr[i] = ptr[i-1]+cnt[i-1];
-        }
-        for (auto x : v){
-            int i = x[p];
-            tmp[ptr[i]] = x;
-            ptr[i]++;
-        }
-        v = tmp;
-    }
-    return;
-}
-
-// a4b26a
-void Build_SA(string s){
-    s += (char)(0);
-    int n = s.size();
-    rnk.resize(n);
-    pos.resize(n);
-
-    vector<array<int, 2>> tmp(n);
-    for (int i=0 ; i<n ; i++) tmp[i] = {s[i], i};
-    sort(tmp.begin(), tmp.end());
-    for (int i=0 ; i<n ; i++) pos[i] = tmp[i][1];
-    rnk[pos[0]] = 0;
-    for (int i=1 ; i<n ; i++){
-        if (tmp[i][0]==tmp[i-1][0]) rnk[pos[i]] = rnk[pos[i-1]];
-        else rnk[pos[i]] = rnk[pos[i-1]]+1;
-    }
-    
-    for (int k=0 ; (1<<k)<n ; k++){
-        vector<array<int, 3>> tmp(n);
-        for (int i=0 ; i<n ; i++) tmp[i] = {rnk[i], rnk[(i+(1<<k))%n], i};
-        Radix_Sort(tmp);
-        for (int i=0 ; i<n ; i++) pos[i] = tmp[i][2];
-        rnk[pos[0]] = 0;
-        for (int i=1 ; i<n ; i++){
-            if (tmp[i][0]==tmp[i-1][0] && tmp[i][1]==tmp[i-1][1]) rnk[pos[i]] = rnk[pos[i-1]];
-            else rnk[pos[i]] = rnk[pos[i-1]]+1;
-        }
-    }
-}
+// b62fc5
+struct SuffixArray {
+	vector<int> sa, lcp;
+	SuffixArray(string& s, int lim = 256) {
+        int n = s.size()+1, k = 0, a, b;
+		vector<int> x(s.begin(), s.end()), y(n), ws(max(n, lim)), rank(n);
+		x.push_back(0);
+        sa = lcp = y;
+        iota(sa.begin(), sa.end(), 0);
+		for (int j=0, p=0 ; p<n ; j=max(1LL, j*2), lim=p) {
+			p = j;
+            iota(y.begin(), y.end(), n-j);
+			for (int i=0 ; i<n ; i++) if (sa[i] >= j) y[p++] = sa[i] - j;
+			fill(ws.begin(), ws.end(), 0);
+			for (int i=0 ; i<n ; i++) ws[x[i]]++;
+			for (int i=1 ; i<lim ; i++) ws[i] += ws[i - 1];
+			for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+			swap(x, y), p = 1, x[sa[0]] = 0;
+			for (int i=1 ; i<n ; i++){
+                a = sa[i - 1];
+                b = sa[i];
+                x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+            }
+		}
+		for (int i=1 ; i<n ; i++) rank[sa[i]] = i;
+		for (int i=0, j ; i<n-1 ; lcp[rank[i++]]=k)
+			for (k && k--, j=sa[rank[i]-1] ; s[i+k]==s[j+k] ; k++);
+		sa.erase(sa.begin());
+		lcp.erase(lcp.begin(), lcp.begin()+2);
+	}
+};
