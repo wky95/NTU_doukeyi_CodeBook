a8a4b576322cc0391543a772f04655fabf0fb1a6
temmie <kapoo950807@gmail.com>
2024-10-27 18:46:20 +0800

    修正錯誤

--- codes/String/KMP.cpp
+++ codes/String/KMP.cpp
@@ -1,17 +1,16 @@
 // 給一個字串 S，定義函數 \pi(i) = k 代表 S[1 ... k] = S[i-k+1 ... i]
-// 61e853
+// 4c61a3
 vector<int> KMP(string &s){
-    int n = s.size();
+    n = SZ(s);
     vector<int> ret(n);
-
+    int now = 0;
     for (int i=1 ; i<n ; i++){
         int j = ret[i-1];
         while (j>0 && s[i]!=s[j]){
-            j = ret[i-1];
+            j = ret[j-1];
         }
         if (s[i]==s[j]) j++;
         ret[i] = j;
     }
-
     return ret;
 }
