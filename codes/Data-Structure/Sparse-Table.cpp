1be66cca76969d9187d92d97dc4bc27fd8a41c48
temmie <kapoo950807@gmail.com>
2024-12-23 19:16:01 +0800

    package as struct

--- codes/Data-Structure/Sparse-Table.cpp
+++ codes/Data-Structure/Sparse-Table.cpp
@@ -1,19 +1,21 @@
-vector<vector<int>> st;
-void build(vector<int> v){
-    int h = __lg(v.size());
-    st.resize(h+1);
-    st[0] = v;
+struct SparseTable{
+    vector<vector<int>> st;
+    void build(vector<int> v){
+        int h = __lg(v.size());
+        st.resize(h+1);
+        st[0] = v;
 
-    for (int i=1 ; i<=h ; i++){
-        int gap = (1<<(i-1));
-        for (int j=0 ; j+gap<st[i-1].size() ; j++){
-            st[i].push_back(min(st[i-1][j], st[i-1][j+gap]));
+        for (int i=1 ; i<=h ; i++){
+            int gap = (1<<(i-1));
+            for (int j=0 ; j+gap<st[i-1].size() ; j++){
+                st[i].push_back(min(st[i-1][j], st[i-1][j+gap]));
+            }
         }
     }
-}
 
-// 回傳 [ll, rr) 的最小值
-int RMQ(int ll, int rr){
-    int h = __lg(rr-ll);
-    return min(st[h][ll], st[h][rr-(1<<h)]);
+    // 回傳 [ll, rr) 的最小值
+    int query(int ll, int rr){
+        int h = __lg(rr-ll);
+        return min(st[h][ll], st[h][rr-(1<<h)]);
+    }
 }
