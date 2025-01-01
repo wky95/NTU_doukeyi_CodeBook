94a643ce25dec5a3a9b393d4635844bf919ceb51
temmie <kapoo950807@gmail.com>
2025-01-02 03:29:15 +0800

    add some comment

--- codes/Misc/Set-Pq-Sort.cpp
+++ codes/Misc/Set-Pq-Sort.cpp
@@ -1,4 +1,4 @@
-// priority_queue
+// priority_queue，務必檢查相等的 case，給所有元素一個排序的依據
 struct cmp{
     bool operator () (Data a, Data b){
         return a.x<b.x;
@@ -6,11 +6,11 @@ struct cmp{
 };
 priority_queue<Data, vector<Data>, cmp> pq;
 
-// set
+// set，務必檢查相等的 case，給所有元素一個排序的依據
 struct Data{
     int x;
 
-    bool operator < (const Data &b){
+    bool operator < (const Data &b) const {
         return x<b.x;
     }
 };
