b6eb6fb33b26f45f0e942eadcc8be0183115fcd9
temmie <kapoo950807@gmail.com>
2025-01-20 23:29:16 +0800

    init

---
+++ codes/Graph/KuhnMunkres.cpp
@@ -0,0 +1,93 @@
+struct KuhnMunkres{
+    int n; // max(n, m)
+    vector<vector<int>> G;
+    vector<int> match, lx, ly, visx, visy;
+    vector<int> slack;
+    int stamp = 0;
+
+    KuhnMunkres(int n) : n(n), G(n, vector<int>(n)), lx(n), ly(n), slack(n), match(n), visx(n), visy(n) {}
+
+    void add(int x, int y, int w){
+        G[x][y] = max(G[x][y], w);
+    }
+
+    bool dfs(int i, bool aug){ // aug = true 表示要更新 match
+        if (visx[i]==stamp) return false;
+        visx[i] = stamp;
+
+        for (int j=0 ; j<n ; j++){
+            if (visy[j]==stamp) continue;
+            int d = lx[i]+ly[j]-G[i][j];
+
+            if (d==0){
+                visy[j] = stamp;
+                if (match[j]==-1 || dfs(match[j], aug)){
+                    if (aug){
+                        match[j] = i;
+                    }
+                    return true;
+                }
+            }else{
+                slack[j] = min(slack[j], d);
+            }
+        }
+        return false;
+    }
+
+    bool augment(){
+        for (int j=0 ; j<n ; j++){
+            if (visy[j]!=stamp && slack[j]==0){
+                visy[j] = stamp;
+                if (match[j]==-1 || dfs(match[j], false)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void relabel(){
+        int delta = INF;
+        for (int j=0 ; j<n ; j++){
+            if (visy[j]!=stamp) delta = min(delta, slack[j]);
+        }
+        for (int i=0 ; i<n ; i++){
+            if (visx[i]==stamp) lx[i] -= delta;
+        }
+        for (int j=0 ; j<n ; j++){
+            if (visy[j]==stamp) ly[j] += delta;
+            else slack[j] -= delta;
+        }
+    }
+
+    int solve(){
+        
+        for (int i=0 ; i<n ; i++){
+            lx[i] = 0;
+            for (int j=0 ; j<n ; j++){
+                lx[i] = max(lx[i], G[i][j]);
+            }
+        }
+
+        fill(ly.begin(), ly.end(), 0);
+        fill(match.begin(), match.end(), -1);
+
+        for(int i = 0; i < n; i++) {
+            fill(slack.begin(), slack.end(), INF);
+            stamp++;
+            if(dfs(i, true)) continue;
+
+            while(augment()==false) relabel();
+            stamp++;
+            dfs(i, true);
+        }
+
+        int ans = 0;
+        for (int j=0 ; j<n ; j++){
+            if (match[j]!=-1){
+                ans += G[match[j]][j];
+            }
+        }
+        return ans;
+    }
+};
