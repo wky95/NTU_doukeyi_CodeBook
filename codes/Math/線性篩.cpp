21184deb6ea11c62ec126f177893ccc9cdb74d5d
temmie <kapoo950807@gmail.com>
2025-01-21 11:37:59 +0800

    update

--- codes/Math/線性篩.cpp
+++ codes/Math/線性篩.cpp
@@ -6,14 +6,14 @@ vector<int> prime, lpf(MAX_N);
 void prime_init(){
     for (int i=2 ; i<MAX_N ; i++){
         if (lpf[i]==0){
-            lpf[i]=i;
+            lpf[i] = i;
             prime.push_back(i);
         }
 
         for (int j : prime){
             if (i*j>=MAX_N) break;
-            lpf[i*j]=j;
-            if (lpf[i]==j) break;
+            lpf[i*j] = j;
+            if (i%j==0) break;
         }
     }
 }
