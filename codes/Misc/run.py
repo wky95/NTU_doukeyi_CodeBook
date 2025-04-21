41e22c691e986d87ef4fdc9ba1193f46eb64b8e0
temmie <kapoo950807@gmail.com>
2025-04-21 22:24:02 +0800

    from os import *

--- codes/Misc/run.py
+++ codes/Misc/run.py
@@ -1,21 +1,21 @@
-import os
+from os import *
 
 f = "pA"
 
 while 1:
     i = input("input: ")
-    p = os.listdir(".")
+    p = listdir(".")
     if i != "":
         f = i
     print(f"file = {f}")
-    if os.system(f"g++ {f}.cpp -std=c++17 -Wall -Wextra -Wshadow -O2 -D LOCAL -g -fsanitize=undefined,address -o {f}"):
+    if system(f"g++ {f}.cpp -std=c++17 -Wall -Wextra -Wshadow -O2 -D LOCAL -g -fsanitize=undefined,address -o {f}"):
         print("CE")
         continue
-    os.system("clear")
+    system("clear")
 
     for x in sorted(p):
         if f in x and ".in" in x:
             print(x)
-            if os.system(f"./{f} < {x}"):
+            if system(f"./{f} < {x}"):
                 print("RE")
             print()
