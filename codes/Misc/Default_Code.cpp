caab25b5e2210c2e7ecd2f084523aa797a846369
Niter <niter060405@gmail.com>
2025-06-15 21:44:08 +0800

    Update Default_Code.cpp
    More debug code!

--- codes/Misc/Default_Code.cpp
+++ codes/Misc/Default_Code.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define debug(HEHE...) std::cerr << #HEHE << " = ", dout(HEHE)
+void dout() { std::cerr << "\n"; }
+template <typename T, typename... U>
+void dout(T t, U... u) { std::cerr << t << ' ', dout(u...); }
 
 void solve(){
 
