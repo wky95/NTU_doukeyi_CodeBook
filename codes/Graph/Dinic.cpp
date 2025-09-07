2af802bff19bf4bc83b001cc5dd4917a08952568
王開育 <80274783+wky95@users.noreply.github.com>
2025-09-07 21:32:51 +0800

    Refactor Dinic's algorithm implementation

--- codes/Graph/Dinic.cpp
+++ codes/Graph/Dinic.cpp
@@ -1,87 +1,60 @@
 // 一般圖：O(EV²)
 // 二分圖：O(E√V)
-struct Flow{
-    using T = int; // 可以換成別的型別
-    struct Edge{
-        int v; T rc; int rid;
-    };
-    vector<vector<Edge>> G;
-    void add(int u, int v, T c){
-        G[u].push_back({v, c, G[v].size()});
-        G[v].push_back({u, 0, G[u].size()-1});
-    }
-    vector<int> dis, it;
-
-    Flow(int n){
-        G.resize(n);
-        dis.resize(n);
-        it.resize(n);
-    }
-
-    // ce56d6
-    T dfs(int u, int t, T f){
-        if (u == t || f == 0) return f;
-        for (int &i=it[u] ; i<G[u].size() ; i++){
-            auto &[v, rc, rid] = G[u][i];
-            if (dis[v]!=dis[u]+1) continue;
-            T df = dfs(v, t, min(f, rc));
-            if (df <= 0) continue;
-            rc -= df;
-            G[v][rid].rc += df;
-            return df;
-        }
-        return 0;
-    }
-
-    // e22e39
-    T flow(int s, int t){
-        T ans = 0;
-        while (true){
-            fill(dis.begin(), dis.end(), INF);
-            queue<int> q;
-            q.push(s);
-            dis[s] = 0;
-
-            while (q.size()){
-                int u = q.front(); q.pop();
-                for (auto [v, rc, rid] : G[u]){
-                    if (rc <= 0 || dis[v] < INF) continue;
-                    dis[v] = dis[u] + 1;
-                    q.push(v);
-                }
-            }
-            if (dis[t]==INF) break;
-
-            fill(it.begin(), it.end(), 0);
-            while (true){
-                T df = dfs(s, t, INF);
-                if (df <= 0) break;
-                ans += df;
-            }
-        }
-        return ans;
-    }
-
-    //  the code below constructs minimum cut
-    void dfs_mincut(int now, vector<bool> &vis){
-        vis[now] = true;
-        for (auto &[v, rc, rid] : G[now]){
-            if (vis[v] == false && rc > 0){
-                dfs_mincut(v, vis);
-            }
-        }
-    }
-
-    vector<pair<int, int>> construct(int n, int s, vector<pair<int,int>> &E){
-        // E is G without capacity
-        vector<bool> vis(n);
-        dfs_mincut(s, vis);
-        vector<pair<int, int>> ret;
-        for (auto &[u, v] : E){
-            if (vis[u] == true && vis[v] == false){
-                ret.emplace_back(u, v);
-            }
-        }
-        return ret;
-    }
+const ll INF = 1 << 30;
+ 
+struct Dinic{
+	struct edge{
+		ll to, cap, flow, rev;
+	};
+	int n,s,t;
+	vector<vector<edge> > g;
+	vector<int> dis,ind;
+	void init(int _n){
+		n = _n;
+		g.assign(n,vector<edge>());
+	}
+	void add_edge(int u,int v,ll cap){
+		g[u].pb(edge{v,cap,0,SZ(g[v])});
+		g[v].pb(edge{u,0,0,SZ(g[u])-1});
+	}
+	bool bfs() {
+		dis.assign(n, -1);
+		queue<int> q;
+		q.push(s), dis[s] = 0;
+		while(!q.empty()){
+			int cur = q.front(); q.pop();
+			for(auto &e : g[cur]) {
+				if(dis[e.to] == -1 && e.flow != e.cap) {
+					q.push(e.to);
+					dis[e.to] = dis[cur] + 1;
+				}
+			}
+		}
+		return dis[t] != -1;
+	}
+	ll dfs(int u, ll cap) {
+		if(u == t || !cap) return cap;
+		for(int &i = ind[u]; i < SZ(g[u]); ++i) {
+			edge & e = g[u][i];
+			if(dis[e.to] == dis[u] + 1 && e.flow != e.cap) {
+				ll df = dfs(e.to, min(e.cap - e.flow, cap));
+				if(df){
+					e.flow += df;
+					g[e.to][e.rev].flow -= df;
+					return df;
+				}
+			}
+		}
+		dis[u] = -1;
+		return 0;
+	}
+	ll maxflow(int _s, int _t) {
+		s = _s; t = _t;
+		ll flow = 0, df;
+		while(bfs()) {
+			ind.assign(n, 0);
+			while((df = dfs(s, INF))) flow += df;
+		}
+		return flow;
+	}
 };
